51685bc86e70d8aacb2900cfdf2aadbbe4cedee5
biancanicolebennett <biancanicolebennett@gmail.com>
2016-09-21 12:53:35 -0400

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,9 @@ var config = {
 
     bio: "I am a UX designer with a background in project management currently residing in New York, New York by way of Kaimuki, Hawaii. On weekends you can find me volunteering at the American Museum of Natural History, making desserts, or surfing.\nI explore, construct, and communicate.",
 
-    email: "biancanicolebennett@gmail.com",
+    email: 
+    "For work samples, CV, or to just say aloha, contact me here:",
+    "biancanicolebennett@gmail.com",
 
     aboutPageLists: {
         "UX Process": [
